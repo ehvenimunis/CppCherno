20983c730a1a60e25683e3d9281bbe54a8000aa5
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-02-13 17:42:32 +0300

    Variables

--- Variables/Variables/Kaynak.cpp
+++ Variables/Variables/Kaynak.cpp
@@ -11,6 +11,11 @@ void ArttirVeYazdir()
 
 } // s_degisken yok edilmedi.
 
+auto topla(int x, int y)
+{
+	return x + y;
+}
+
 int main()
 {
 	// declaration and definition
@@ -30,12 +35,44 @@ int main()
 	int _c, _d45, e;
 
 	// Let us print a variable
-	cout << a123 << endl;
+	cout << "Declerated Variable : " << a123 << endl;
 
+
+	//STATIC
 	std::cout << "\nStatic variable values : " << std::endl;
 	ArttirVeYazdir();
 	ArttirVeYazdir();
 	ArttirVeYazdir();
+
+	// AUTO
+	auto x{5.6}; //Compailer know type of x is double!
+	std::cout << "\nAuto value :" << x << std::endl;
+	std::cout << "5 + 5 = " << topla(5, 5) << std::endl; //Return type can auto for function!
+
+	// Ýkilik, sekizlik ve on altýlýk sayý sabitleri
+	int aa = 0b01011001;
+	int bb = 0765;
+	int cc = 0xFF;
+
+
+	// CONST
+	const double gravity{ 9.8 }; // Dogrudur ve tercih edilir.
+	double const PI{ 3.14 }; // Calisir ama onerilmez.
+
+	int age = 55;
+	const int userAgeVarible(age); // Defines and get first value 
+
+
+	// CHANGE TYPE
+	int myInt{ 12 };
+	long myLong;
+	myLong = myInt;
+
+	// static_cast
+	char c = 'a';
+	std::cout << "\nStatic Cast :" << std::endl << static_cast<char>(c) << std::endl; // a yazdýrýr.
+	std::cout << static_cast<int>(c) << std::endl; // a deðil 97 yazdýrýr.
+
 
 	return 0;
 }
