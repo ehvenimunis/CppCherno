315c52779164c49c87fc094f9784a14c92ee90f2
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-02-27 16:49:31 +0300

    Loops

---
+++ Loops/Loops.cpp
@@ -0,0 +1,31 @@
+﻿// Loops.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    string c[] = { "","one","two","three","four","five","six","seven","eight","nine" };
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
+        cout << ((i <= 9) ? c[i] : ((i % 2 == 0) ? "even" : "odd")) << endl;
+    return 0;
+}
+
+
+/*
+    Sample Input
+
+    8
+    11
+
+    Sample Output
+
+    eight
+    nine
+    even
+    odd
+*/
+
