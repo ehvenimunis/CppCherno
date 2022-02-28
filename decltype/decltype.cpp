9e45a557de7757412b80c634e931ebf9f2a0fb1a
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-02-28 23:52:33 +0300

    Decltype

---
+++ decltype/decltype.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+#include <type_traits>
+
+class A
+{
+public:
+    int memberFunction(double);
+    double x;
+};
+const A* a;
+
+// y'nin tipi double (nasil tanimlandiysa oyle)
+decltype(a->x) y;
+
+// Parantez kullanımı: 
+// z'nin tipi double&. Neden boyle? Cunku ekstra eklenen parantezler
+// ifadeyi l value expression'a ceviriyor
+decltype((a->x)) z = y;
+
+// Asagida donus tipi de, template parametrelerinin tipinden otomatik olarak belirlenebiliyor
+// Donus degerinin otomatik atanmasi C++ 14 ile sunuluyor, aklinizda bulunsun
+template<typename T, typename U>
+auto add(T t, U u) -> decltype(t + u)
+
+{
+    return t + u;
+}
+
+int main()
+{
+    int i = 33;
+    decltype(i) j = i * 2;
+
+    // Konteynerler ile de kullanabilirsiniz
+    // Bu durumda: vector<double>
+    std::vector<decltype(a->x)> vi;
+
+    // Sinif uyesi fonksiyonlarin tipleri icin de kullanabilirsiniz
+    // int A::(*)(double)
+    using memberFunctionPointer = decltype(&A::memberFunction);
+
+    std::cout << "i = " << i << ", "
+        << "j = " << j << '\n'
+        << "Toplama (int, float): " << add<float, int>(10.5F, 5) << '\n'
+        << "Toplama (int, int): " << add<int, int>(10.5F, 5) << '\n';
+
+    auto f = [](int a, int b) -> int
+    {
+        return a * b;
+    };
+
+    // Benzer sekilde lambda tiplerini de bu anlamda kullanabiliyoruz
+    decltype(f) g = f;
+    i = f(2, 2);
+    j = g(3, 3);
+
+    std::cout << "i = " << i << ", "
+        << "j = " << j << '\n';
+
+    return 0;
+}
+
+
+/*
+
+    i = 33, j = 66
+    Toplama (int, float): 15.5
+    Toplama (int, int): 15
+    i = 4, j = 9
+
+*/
---
+++ decltype1/decltype1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <type_traits>
+#include <cstdint>
+
+int main()
+{
+    int i = 33;
+    decltype(i) j = i * 2;
+    std::cout << std::boolalpha;
+
+    std::cout << "i and j same type? "
+        << std::endl << std::is_same_v<decltype(i), decltype(j)> << '\n';
+
+    // int 32 bit ise true
+    std::cout << std::endl << std::is_same<int, std::int32_t>::value << ' ' << '\n';
+
+    // dogru degil
+    std::cout << std::is_same<int, short>::value << ' ' << '\n';
+
+    // simdi bir de degiskenleri karsilastiralim
+    long double num1 = 1.0;
+    long double num2 = 2.0;
+    std::cout << std::is_same_v<decltype(num1), decltype(num2)> << '\n';
+
+    return 0;
+}
+
+
+/*
+
+i and j same type?
+true
+
+true
+false
+true
+
+*/
