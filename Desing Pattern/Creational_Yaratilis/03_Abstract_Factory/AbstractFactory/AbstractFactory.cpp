fa591e55e1b756665fed6a1f780fbf51a4e64e8f
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-04-17 14:56:28 +0300

    Abstract Factory topic

---
+++ Desing Pattern/Creational_Yaratilis/03_Abstract_Factory/AbstractFactory/AbstractFactory.cpp
@@ -0,0 +1,82 @@
+﻿//============================================================================
+// İsim        : AbstractFactory
+// Yazan       : Muhammed Salih Aydoğan
+// Version     : 1.0
+// Copyright   : 
+// Açıklama    : Abstract Factory Pattern
+//============================================================================
+#include <iostream>
+using namespace std;
+
+#define DOLAP 1
+//#define MASA 1
+
+class Marangoz
+{
+public:
+    virtual void urunTip() = 0; /* override tanımlayıcısını kullanacaksak, burada fonksiyonun virtual olduğunu belirtmek zorundayız */
+};
+
+class Masa : public Marangoz
+{
+public:
+    void urunTip() override /* yalnızca virtual fonksiyonları override edebiliriz */
+    {
+        std::cout << "Masa Yapim Onarim" << std::endl;
+    }
+};
+
+class Dolap : public Marangoz
+{
+public:
+    void urunTip() override
+    {
+        std::cout << "Dolap Yapim Onarim" << std::endl;
+    }
+};
+
+class Fabrika
+{
+public:
+    virtual Marangoz* fabrikaOlustur() = 0; /* override tanımlayıcısını kullanacaksak, burada fonksiyonun virtual olduğunu belirtmek zorundayız */
+};
+
+class DolapOlustur : public Fabrika
+{
+public:
+    Marangoz* fabrikaOlustur() override
+    {
+        return new Dolap;
+    }
+};
+
+class MasaOlustur : public Fabrika
+{
+public:
+    Marangoz* fabrikaOlustur() override
+    {
+        return new Masa;
+    }
+};
+
+int main()
+{
+    #ifdef DOLAP
+    Fabrika* fabrika = new DolapOlustur;
+    #elif MASA
+    Fabrika* fabrika = new MasaOlustur;
+    #endif
+
+    Marangoz* marangoz = fabrika->fabrikaOlustur();
+    marangoz->urunTip();
+
+    delete[] fabrika;
+    delete[] marangoz;
+    return 0;
+}
+
+/*
+    Ortak bir sınıf türünden türetilecek, farklı nesneler oluşturduk.
+    Bu pattern, genel bir deseni takip eden nesneler oluşturmamıza izin veriyor.
+    Somut sınıflarını doğrudan belirtmeden, ilgili veya bağımlı nesnelerin ailelerinin oluşturulmasını sağladı.
+*/
