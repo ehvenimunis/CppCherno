287c38c3c109dae0ebead5c097d0db02ce03b715
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-05-26 20:30:03 +0300

    Added Commend

--- std/uniqe_ptr/unique_ptrB/unique_ptrB.cpp
+++ std/uniqe_ptr/unique_ptrB/unique_ptrB.cpp
@@ -19,11 +19,13 @@ int main()
     unique_ptr<A> up1(new A);
     //Yeni bir unique_ptr nesnesi dinamik ömürlü bir nesneyle başlatılıyor.
     unique_ptr<A> up2(new A);
+    cout << "Before -> up2 value : " << up2 << endl;
+    cout << "Before -> up1 value : " << up1 << endl;
     up2 = move(up1); // taşıyan atama işlevi ile up2'nin daha önce sahip olduğu nesne sonlandırılır.
     // Sahiplik up1'den up2'ye devredilir.
 
-    cout << "up2 value : " << up2 << endl;
-    cout << "up1 value : " << up1 << endl; // Yeni bir sahiplik edinmeden sahip olduğu nesneyi bırakan bir unique_ptr nesnesi hiçbir nesneyi göstermez. 
+    cout << "After  -> up2 value : " << up2 << endl;
+    cout << "After  -> up1 value : " << up1 << endl; // Yeni bir sahiplik edinmeden sahip olduğu nesneyi bırakan bir unique_ptr nesnesi hiçbir nesneyi göstermez. 
 
 
 
@@ -32,3 +34,10 @@ int main()
     // ptr = new A; // Geçersiz
     ptr = unique_ptr<A>(new A); // Geçerli. Eski nesne sonlandırılır yenisi sahiplenilir. 
 }
+
+/*
+    Before -> up2 value : 0000026CD8E1EBE0
+    Before -> up1 value : 0000026CD8E1E760
+    After  -> up2 value : 0000026CD8E1E760
+    After  -> up1 value : 0000000000000000
+*/
