6cde5e1c01d112fc17b336f3303aa8c43d5cb829
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-05-29 10:14:00 +0300

    Added make_unique topic

---
+++ std/Smart Pointer/uniqe_ptr/make_uniqueA/make_uniqueA.cpp
@@ -0,0 +1,23 @@
+﻿// make_uniqueA.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <memory>
+#include <string>
+#include <iostream>
+int main()
+{
+
+	auto up = std::make_unique<std::string>(10, 'A'); // İşleve kontrol edilecek dinamik nesnenin kurucu işlevinin kullanacağı argümanlar gönderiliyor.
+	std::cout << *up << "\n";
+	//...
+
+	/*
+	Yukarıdaki kodda make_unique işlev şablonundan üretilecek bir işlevle string sınıfının size_t ve char parametreli kurucu işlevine 10 ve ‘A’ değerleri gönderilerek önce dinamik ömürlü bir string nesnesi hayata getiriliyor. Daha sonra, hayata gelen nesneyi kontrol eden bir unique_ptr nesnesi geri döndürülüyor.
+	*/
+}
+
+/*
+
+AAAAAAAAAA
+
+*/
