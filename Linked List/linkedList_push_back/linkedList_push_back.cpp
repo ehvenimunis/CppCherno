8e14027ff9845e9c0691cac196ecadaa7de3a70c
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-05-31 23:22:49 +0300

    Added traverse method in linked list

--- Linked List/linkedList_push_back/linkedList_push_back.cpp
+++ Linked List/linkedList_push_back/linkedList_push_back.cpp
@@ -19,24 +19,48 @@ public:
 	Node* begin()const { return root; }
 	Node* end()const { return NULL; }
 	//bool isEmty()const { return root == NULL;  }
-	bool isEmty()const { return root == end(); }
+	bool isEmty()const { return begin() == end(); }
 
 	void push_back(const int& value) {
 		if (isEmty())
 			root = new Node(value);
 		else {
 			Node* tmp = begin();
-			while (tmp = begin())
+			while (tmp->next != end())
 				tmp = tmp->next;
 			tmp->next = new Node(value);
 		}
 	}	
+
+	void traverse() {
+		Node* first = begin();
+		while (first != end()) {
+			cout << first->data << " ";
+			first = first->next;
+		}
+		cout << endl;
+	}
 };
+
 
 int main() {
 	list list;
 	cout << list.isEmty() << endl; // Get 1 value for empty
-	list.push_back(1453);
+	list.push_back(153);
 	cout << list.isEmty() << endl; 
+	list.push_back(154);
+	list.push_back(155);
+	list.push_back(156);
+	list.push_back(157);
+	list.push_back(158);
+
+	list.traverse();
+
 	return 0;
 }
+
+/*
+	1
+	0
+	153 154 155 156 157 158
+*/
