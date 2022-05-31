732082aa8626bf98e4ebd44f44c6f0aab55f6c59
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-05-31 20:36:59 +0300

    Added push_back method for linked list

---
+++ Linked List/linkedList_push_back/linkedList_push_back.cpp
@@ -0,0 +1,42 @@
+﻿// linkedList_push_back.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+class Node {
+	public:
+		Node(const int& data = 0, Node* next = NULL) : data(data), next(next) { } // consructor
+
+		int data;
+		Node* next;
+};
+
+class list {
+	Node* root;
+public:
+	list() : root(NULL){ }
+	Node* begin()const { return root; }
+	Node* end()const { return NULL; }
+	//bool isEmty()const { return root == NULL;  }
+	bool isEmty()const { return root == end(); }
+
+	void push_back(const int& value) {
+		if (isEmty())
+			root = new Node(value);
+		else {
+			Node* tmp = begin();
+			while (tmp = begin())
+				tmp = tmp->next;
+			tmp->next = new Node(value);
+		}
+	}	
+};
+
+int main() {
+	list list;
+	cout << list.isEmty() << endl; // Get 1 value for empty
+	list.push_back(1453);
+	cout << list.isEmty() << endl; 
+	return 0;
+}
