944fcde188344f140f292c08595542da416cfbb6
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-06-09 00:09:09 +0300

    Unchecked exception

---
+++ Exception/uncheckedException/uncheckedException.cpp
@@ -0,0 +1,32 @@
+﻿// uncheckedException.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+#include <iostream>
+using namespace std;
+
+// This function signature is fine by the compiler, but not recommended.
+// Ideally, the function should specify all uncaught exceptions and function
+// signature should be "void fun(int *ptr, int x) throw (int *, int)"
+void fun(int* ptr, int x)
+{
+	if (ptr == NULL)
+		throw ptr;
+	if (x == 0)
+		throw x;
+	/* Some functionality */
+}
+
+int main()
+{
+	try {
+		fun(NULL, 0);
+	}
+	catch (...) {
+		cout << "Caught exception from fun()";
+	}
+	return 0;
+}
+
+/*
+
+Caught exception from fun()
+
+*/
