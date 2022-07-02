67753069c914cba0a591f28349355b3f7ab1b36d
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-07-02 15:03:12 +0300

    Added facede desing pattern topic

---
+++ Desing Pattern/Structural_Yapisal/05_Facade/FacadePattern/FacadePattern.cpp
@@ -0,0 +1,109 @@
+﻿//============================================================================
+// İsim        : 05_Facade
+// Yazan       : Muhammed Salih Aydoğan     
+// Version     : 1.0
+// Copyright   : 
+// Açıklama    : Facade Pattern
+//============================================================================
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+/* Operasyonları tanımlıyoruz. Biz projemizde "Alarm", "Elektrik", "Tv" işlemleri tanımladık.*/
+class Alarm
+{
+public:
+    void alarmAc()
+    {
+        cout << "Alarm Open. Your home safety..." << endl;
+    }
+
+    void alarmKapat()
+    {
+        cout << "Alarm Closed..." << endl;
+    }
+};
+
+class Elektrik
+{
+public:
+    void elektrikAc()
+    {
+        cout << "Electric Open..." << endl;
+    }
+
+    void elektrikKapat()
+    {
+        cout << "Electric Close..." << endl;
+    }
+};
+
+class Tv
+{
+public:
+    void tvAc()
+    {
+        cout << "Tv Open..." << endl;
+    }
+
+    void tvKapat()
+    {
+        cout << "TV Close..." << endl;
+    }
+};
+
+/* 
+Bu sınıf içinde oluşturduğumuz "Alarm", "Elektrik", "Tv" sınıflarından nesneler yarattık. Bu sınıfımızda iki fonksiyonumuz bulunmakta. İlki "evdenDisariCikinca" methodu, ilk belirlediğimiz sınıflardan, evden çıkınca yapılacak olan işlemleri tanımlamakta. Diğeri "eveDonunce" methodu eve geri dönünce yapıalcak olan işlemleri barındırmaktadır.
+*/
+class EvFacade
+{
+    Alarm alarm;
+    Elektrik elektrik;
+    Tv tv;
+
+public:
+    EvFacade() {}
+
+    void evdenDisariCikinca()
+    {
+        elektrik.elektrikKapat();
+        tv.tvKapat();
+        alarm.alarmAc();
+    }
+
+    void eveDonunce()
+    {
+        alarm.alarmKapat();
+        elektrik.elektrikAc();
+        tv.tvAc();
+    }
+};
+
+int main()
+{ 
+    /* Bir alt sistemin parçalarını oluşturan classları istemciden soyutlayarak kullanımı daha da kolaylaştırıyoruz */
+    EvFacade evFacade;
+
+    cout << "Evden Gidiliyor ..." << endl;
+    evFacade.evdenDisariCikinca();
+
+    cout << "---------------------------------------------" << endl;
+
+    cout << "Eve Giriliyor..." << endl;
+    evFacade.eveDonunce();
+}
+
+
+/*
+
+    Evden Gidiliyor ...
+    Electric Close...
+    TV Close...
+    Alarm Open. Your home safety...
+    ---------------------------------------------
+    Eve Giriliyor...
+    Alarm Closed...
+    Electric Open...
+    Tv Open...
+*/
--- Desing Pattern/Structural_Yapisal/05_Facade/README.md
+++ Desing Pattern/Structural_Yapisal/05_Facade/README.md
@@ -1,6 +1,9 @@
-# Facade Pattern
+# Facade Design Pattern
 
 **"Facade Pattern"** pek çok sınıftan oluşan karmaşık bir sistemin karmaşıklığını saklamak ve istemci daha basit ve kolay kullanılabilir bir arayüz sunmak için kullanılır. Yapısal bir örüntüdür. Basitçe bu örüntü sisteme bir arayüz ekler ve sistemin karmaşıklığını giderir. 
+Bir alt sistemin parçalarını oluşturan classları istemciden soyutlayarak kullanımı daha da kolaylaştırmak için tasarlanmış tasarım kalıbıdır. Mimarisel açıdan ise, karmaşık ve detaylı bir sistemi organize eden ve bir bütün olarak clientlara(istemcilere) sunan yapıdır. Anlayacağınız, karmaşık ve detaylı olarak nitelendirdiğimiz bu sistemi bir alt sistem olarak varsayarsak eğer bu sistemi kullanacak clientlara daha basit bir arayüz sağlamak ve alt sistemleri bu arayüze organize bir şekilde dahil etmek ve bu alt sistemlerin sağlıklı çalışabilmesi için bu arayüz çatısı altında işin algoritmasına uygun işlev sergilemek istersek Facade Design Pattern’i kullanmaktayız.
+Burada bilmeniz gereken durum, alt sistem içerisinde bulunan sınıfların birbirlerinden bağımsız olmasıdır. Ayriyetten Facade sınıfından da bağımsız bir şekilde çalışabilmektedirler.
+Facade bizim classlarımızı içermek zorundadır ve operasyonu yaparken onlara ait fonksiyonellikleri kullanması gerekli.
 
 ## Facade Pattern Kullanım Adımları
 
