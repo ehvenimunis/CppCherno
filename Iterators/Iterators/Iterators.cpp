a954d96ae675ef02134d6a41ec46b108256a27aa
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-07-24 11:25:18 +0300

    Added iterator topic

---
+++ Iterators/Iterators/Iterators.cpp
@@ -0,0 +1,63 @@
+﻿// Iterators.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+int main()
+{
+    std::vector<int> values = { 1, 2, 3, 4, 5 };
+
+    for (int i = 0; i < values.size(); i++) {
+        std::cout << values[i] << " ";
+    }
+    std::cout << std::endl;
+
+    /********************************************************************************/
+    for (int value : values)
+        std::cout << value << " ";
+    std::cout << std::endl;
+
+    /********************************************************************************/
+    for (std::vector<int>::iterator it = values.begin(); 
+        it != values.end(); it++) 
+    {
+        std::cout << *it << " " ;
+    }
+
+    /********************************************************************************/
+    using ScoreMap = std::unordered_map<std::string, int>;
+    //using ScoreMapConstIter = ScoreMap::const_iterator;
+    ScoreMap map;
+    map["Salih"] = 10;
+    map["Mery"] = 100;
+
+    std::cout << std::endl;
+    for (ScoreMap::const_iterator it = map.begin(); it != map.end(); it++)
+    {
+        auto& key = it->first;
+        auto& value = it->second;
+        std::cout << key << " = " << value << std::endl;
+    }
+
+    std::cout << std::endl;
+    for (auto kv : map) {
+        auto& key = kv.first;
+        auto& value = kv.second;
+
+        std::cout << key << " = " << value << std::endl;
+    }
+
+    std::cout << std::endl;
+    for (auto [a, b] : map)
+        std::cout << a << " = " << b << std::endl;
+
+    /********************************************************************************/
+
+
+
+    std::cin.get();
+
+}
+
---
+++ Iterators/next_and_prev/next_and_prev.cpp
@@ -0,0 +1,30 @@
+﻿// next_and_prev.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
+int main()
+{
+	std::list<int> ilist{ 5, 56, 7, 48, 11, 23, 37, 49 };
+	for_each(next(ilist.begin()), prev(ilist.end()), [](int& r) {r *= 2; });
+
+	/*
+		next(ilist.begin()) ifadesi ile ilist kabında tutulan ikinci öğenin konumunu
+		prev(ilist.end()) ifadesi ile kapta tutulan son öğenin konumunu elde ediyoruz. 
+		
+		for_each algoritmasıyla ilist listesindeki ilk ve son öğe haricindeki tüm öğeleri 2 katına çıkartmış olduk.
+	*/
+
+	for (int i : ilist)
+		std::cout << i << " ";
+
+	std::cin.get();
+}
+
+/*
+
+	5 112 14 96 22 46 74 49
+
+*/
